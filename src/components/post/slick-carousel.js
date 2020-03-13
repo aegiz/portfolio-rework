75fbe9de217d5d374ecbb4e01bd2f7ddbebfe027
Adrien Rahier <adrien.rahier@gmail.com>
2020-03-13 12:50:23 +0100

    Simple demo carousel

--- src/components/post/slick-carousel.js
+++ src/components/post/slick-carousel.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Slider from "react-slick";
+import Img from "gatsby-image";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const SlickCarousel = ({ text, size, bkgdColor, settings, images }) => (
+const SlickCarousel = ({ settings, images }) => (
 	<Slider {...settings}>
 		{images.map((image, i) => (
 			<div key={i}>
-				<img width="350" src={`${image}`} alt="Lake 11"></img>
+				<img width="350" src={`${image.src}`} alt={`${image.alt}`}></img>
 			</div>
 		))}
 	</Slider>
