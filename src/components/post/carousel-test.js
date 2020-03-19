21763e75366876c602f35fc355a233940278d9a1
Adrien Rahier <adrien.rahier@gmail.com>
2020-03-19 10:45:38 +0100

    Working on the gallery

--- src/components/post/carousel-test.js
+++ src/components/post/carousel-test.js
@@ -8,15 +8,51 @@ import CTAicon from "./icons";
 // Styles
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import styled, { withTheme } from "styled-components";
+import styled from "styled-components";
 
-const MajorContainer = styled.div`
+const MainContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	max-width: 1000px;
+	margin: 0 auto;
+`;
+
+const Thumbnails = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	align-items: flex-start;
+	justify-content: flex-start;
+	width: 100%;
+	@media (max-width: 1023px) {
+		display: none;
+	}
+`;
+
+const Thumbnail = styled.div`
+	width: 100px;
+	height: 100px;
+	padding: 0 10px;
+	cursor: pointer;
+	img {
+		border: ${props =>
+			props.current != props.index ? "none" : "1px solid red"};
+		opacity: 1;
+		width: 100%;
+		object-fit: cover;
+		transition: all 0.3s;
+		&:hover {
+			opacity: 0.65;
+		}
+	}
+`;
+
+const CarouselContainer = styled.div`
 	display: flex;
 	flex-direction: row;
 	width: 100%;
 	height: 500px;
-	max-width: 1000px;
-	margin: 0 auto;
 	@media (max-width: 1023px) {
 		height: auto;
 		flex-direction: column-reverse;
@@ -37,10 +73,15 @@ const Loading = styled.div`
 `;
 
 const ContainerSliderLeft = styled.div`
-	display: ${props => (props.hide ? "none" : "block")};
-	width: 320px;
-	background: #5454aa;
-	padding: 0.25em 1em;
+	display: none;
+	@media (max-width: 1023px) {
+		display: block;
+		padding: 0.25em 1em;
+		width: 100%;
+	}
+	@media (max-width: 480px) {
+		display: none;
+	}
 	.slider-item {
 		opacity: 0.8;
 		transition: all 0.3s;
@@ -57,18 +98,11 @@ const ContainerSliderLeft = styled.div`
 		opacity: 1;
 		transform: scale(1.1);
 	}
-	@media (max-width: 1023px) {
-		width: 100%;
-	}
-	@media (max-width: 480px) {
-		display: none;
-	}
 `;
 
 const ContainerSliderRight = styled.div`
 	position: relative;
 	width: 620px;
-	background: #5454bb;
 	.slider-item img {
 		margin: 0 auto;
 		width: 100%;
@@ -96,13 +130,19 @@ const IconContainter = styled.div`
 	position: absolute;
 	bottom: 0;
 	right: 0;
-	cursor: pointer;
+	background: rgba(255, 255, 255, 0.3);
 	img {
+		opacity: 1;
 		padding: 0 10px;
+		transition: all 0.3s;
+		cursor: pointer;
+		&:hover {
+			opacity: 0.8;
+		}
 	}
 `;
 
-export default class VerticalMode extends Component {
+export default class CustomCarousel extends Component {
 	state = {
 		slick1: null,
 		slick2: null,
@@ -129,8 +169,8 @@ export default class VerticalMode extends Component {
 			vertical: true,
 			centerMode: true,
 			onInit: () => this.setState(state => ({ sliderLeftLoaded: true })),
-			afterChange: (current, next) =>
-				this.setState(state => ({ currentSlide: current })),
+			beforeChange: (current, next) =>
+				this.setState(state => ({ currentSlide: next })),
 			responsive: [
 				{
 					breakpoint: 1023,
@@ -158,8 +198,8 @@ export default class VerticalMode extends Component {
 			slidesToScroll: 1,
 			autoplaySpeed: 300,
 			onInit: () => this.setState(state => ({ sliderRightLoaded: true })),
-			afterChange: (current, next) =>
-				this.setState(state => ({ currentSlide: current })),
+			beforeChange: (current, next) =>
+				this.setState(state => ({ currentSlide: next })),
 			responsive: [
 				{
 					breakpoint: 480,
@@ -174,86 +214,101 @@ export default class VerticalMode extends Component {
 
 		return (
 			<>
-				<MajorContainer>
-					<Loading
-						sliderLoaded={
-							this.state.sliderLeftLoaded && this.state.sliderRightLoaded
-						}
-					>
-						Carousel is loading...
-					</Loading>
-					<ContainerSliderLeft hide={this.props.images.length < 4}>
-						<Slider
-							asNavFor={this.state.slick2}
-							ref={slider => (this.slider1 = slider)}
-							{...settingsSliderLeft}
+				<MainContainer>
+					<Thumbnails>
+						{this.props.images.map((image, i) => (
+							<Thumbnail
+								className="thumbnail"
+								onClick={e => this.state.slick2.slickGoTo(i)}
+								key={i}
+								index={i}
+								current={this.state.currentSlide}
+							>
+								<img src={`${image.src}`} alt={`${image.alt}`}></img>
+							</Thumbnail>
+						))}
+					</Thumbnails>
+					<CarouselContainer>
+						<Loading
+							sliderLoaded={
+								this.state.sliderLeftLoaded && this.state.sliderRightLoaded
+							}
 						>
-							{this.props.images.map((image, i) => (
-								<div
-									className="slider-item"
-									onClick={e => this.state.slick2.slickGoTo(i)}
-									key={i}
-								>
-									<img src={`${image.src}`} alt={`${image.alt}`}></img>
-								</div>
-							))}
-						</Slider>
-					</ContainerSliderLeft>
-					<ContainerSliderRight>
-						<Slider
-							asNavFor={this.state.slick1}
-							ref={slider => (this.slider2 = slider)}
-							{...settingsSliderRight}
-						>
-							{this.props.images.map((image, i) => (
-								<div className="slider-item" key={i}>
-									<img src={`${image.src}`} alt={`${image.alt}`}></img>
-								</div>
-							))}
-						</Slider>
-						<div className="photos-counter">
-							<span>{this.state.currentSlide + 1}</span>
-							<span>/</span>
-							<span>{this.state.totalSlide}</span>
-						</div>
-						<IconContainter>
-							<CTAicon
-								type={"previous"}
-								alt={"previous icon"}
-								width={24}
-								height={24}
-								onClick={() => this.state.slick2.slickPrev()}
-							/>
-							<CTAicon
-								type={"next"}
-								alt={"next icon"}
-								width={24}
-								height={24}
-								onClick={() => this.state.slick2.slickNext()}
-							/>
-							<CTAicon
-								type={this.state.autoplay ? "pause" : "play"}
-								alt={"play icon"}
-								width={32}
-								height={32}
-								onClick={() => {
-									// This doesn't work
-									if (this.state.autoplay) {
-										this.state.slick2.slickPause();
-										this.setState({
-											autoplay: false,
-										});
-									} else {
-										this.state.slick2.slickPlay();
-										this.setState({
-											autoplay: true,
-										});
-									}
-								}}
-							/>
-						</IconContainter>
-					</ContainerSliderRight>
-				</MajorContainer>
+							Carousel is loading...
+						</Loading>
+						<ContainerSliderLeft hide={this.props.images.length < 4}>
+							<Slider
+								asNavFor={this.state.slick2}
+								ref={slider => (this.slider1 = slider)}
+								{...settingsSliderLeft}
+							>
+								{this.props.images.map((image, i) => (
+									<div
+										className="slider-item"
+										onClick={e => this.state.slick2.slickGoTo(i)}
+										key={i}
+									>
+										<img src={`${image.src}`} alt={`${image.alt}`}></img>
+									</div>
+								))}
+							</Slider>
+						</ContainerSliderLeft>
+						<ContainerSliderRight>
+							<Slider
+								asNavFor={this.state.slick1}
+								ref={slider => (this.slider2 = slider)}
+								{...settingsSliderRight}
+							>
+								{this.props.images.map((image, i) => (
+									<div className="slider-item" key={i}>
+										<img src={`${image.src}`} alt={`${image.alt}`}></img>
+									</div>
+								))}
+							</Slider>
+							<div className="photos-counter">
+								<span>{this.state.currentSlide + 1}</span>
+								<span>/</span>
+								<span>{this.state.totalSlide}</span>
+							</div>
+							<IconContainter>
+								<CTAicon
+									type={"previous"}
+									alt={"previous icon"}
+									width={24}
+									height={24}
+									onClick={() => this.state.slick2.slickPrev()}
+								/>
+								<CTAicon
+									type={"next"}
+									alt={"next icon"}
+									width={24}
+									height={24}
+									onClick={() => this.state.slick2.slickNext()}
+								/>
+								<CTAicon
+									type={this.state.autoplay ? "pause" : "play"}
+									alt={"play icon"}
+									width={32}
+									height={32}
+									onClick={() => {
+										// This doesn't work
+										if (this.state.autoplay) {
+											this.state.slick2.slickPause();
+											this.setState({
+												autoplay: false,
+											});
+										} else {
+											this.state.slick2.slickPlay();
+											this.setState({
+												autoplay: true,
+											});
+										}
+									}}
+								/>
+							</IconContainter>
+						</ContainerSliderRight>
+					</CarouselContainer>
+				</MainContainer>
 			</>
 		);
 	}
