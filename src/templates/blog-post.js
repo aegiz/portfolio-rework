77fd645921ffd60f36970440fa0f842864734fd2
Adrien Rahier <adrien.rahier@gmail.com>
2020-04-30 11:18:32 +0200

    Test netlify 3

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,7 +5,7 @@ import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
 // Components
-// import Cover from "@components/post/cover";
+import Cover from "@components/post/cover";
 import CTAhome from "@components/post/CTAhome";
 import CTAotherProject from "@components/post/CTAotherProject";
 import Layout from "@components/layout";
@@ -182,11 +182,11 @@ class BlogPostTemplate extends React.Component {
 							)}
 						</LeftPanelTop>
 						<LeftPanelIntro>
-							{/* {this.props.isM && (
+							{this.props.isM && (
 								<Cover
 									src={post.frontmatter.featuredImage.childImageSharp.fluid}
 								/>
-							)} */}
+							)}
 							<div className="upper">
 								<Title>{post.frontmatter.title}</Title>
 								<Description windowHeight={this.props.windowHeight}>
@@ -213,12 +213,12 @@ class BlogPostTemplate extends React.Component {
 						<MDXRenderer>{post.body}</MDXRenderer>
 					)}
 					<RightPanel>
-						{/* {post.frontmatter.typeOfArticle === "multistep" &&
+						{post.frontmatter.typeOfArticle === "multistep" &&
 							!this.props.isM && (
 								<Cover
 									src={post.frontmatter.featuredImage.childImageSharp.fluid}
 								/>
-							)} */}
+							)}
 						{post.frontmatter.typeOfArticle === "singlestep" && (
 							<MDXRenderer>{post.body}</MDXRenderer>
 						)}
